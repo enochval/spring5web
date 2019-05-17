7f5916388e2dcfed8892976e6bf68b6ca979c43b
Osarenren Enoch <osarenrenenoch@gmail.com>
2019-05-17 15:57:03 +0100

    adding JPA entities

---
+++ src/main/java/com/example/spring5/web/spring5Web/Spring5WebApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring5.web.spring5Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebApplication.class, args);
+	}
+
+}
