3bd37d951bd31e30a8fde0ff396abb4f54a6b68f
Enoch Osarenren <osarenrenenoch@gmail.com>
2024-01-25 22:30:31 +0100

    introduction to thymeleaf templating

---
+++ src/main/java/com/example/spring5/web/spring5Web/controllers/AuthorController.java
@@ -0,0 +1,21 @@
+package com.example.spring5.web.spring5Web.controllers;
+
+import com.example.spring5.web.spring5Web.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
